a251b122c8a93a60cecb4d2202b4028d6a8ec37c
jht96 <118475844+jht96@users.noreply.github.com>
2023-11-16 15:37:25 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,6 +25,9 @@ app.get('/album_songs/:album_id', routes.album_songs);
 app.get('/top_songs', routes.top_songs);
 app.get('/top_albums', routes.top_albums);
 app.get('/search_songs', routes.search_songs);
+app.get('/author', routes.author);
+app.get('/author/:genre', routes.genre_authors);
+app.get('/author/:author', routes.author_top);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
